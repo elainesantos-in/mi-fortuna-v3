2c5c499dd27c44c0255219a559b3f9106051c9e8
Elaine Santos <elaineleonidossantos@gmail.com>
2026-03-09 22:31:05 -0300

    Criar tela de login

---
+++ frontend/mifortuna-front/src/app/components/Input.jsx
@@ -0,0 +1,7 @@
+
+export default function Input(props){
+    return(
+        <input className="w-full bg-[#E5F1DF] py-1 rounded outline-none focus:bg-white focus:border focus:border-[#78BC5F]" {...props} />
+    )
+    
+}
